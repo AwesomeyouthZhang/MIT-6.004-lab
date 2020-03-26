7a718f9df53886215c87776ab600e73a2926005d
AwesomeyouthZhang <zhang.zx.1073538693@gmail.com>
2020-03-26 16:22:41 +0800

    update Lab 1

---
+++ Lab1/triangular.s
@@ -0,0 +1,14 @@
+
+li a1, 0		# initialize a0(sum) by 0
+
+li t1, 0		# initialize t1(i) by 0
+li t2, 4
+
+lw t0, 0(t4)
+loop:
+ecall # print integer ecall
+add a1, a1, t1
+ecall # terminate ecallecall #
+addi t1, t1, 1
+
+bgt  t2, t1, loop
